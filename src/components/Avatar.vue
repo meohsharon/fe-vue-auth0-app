deaab9756898cdcd5e77908745b50350631468a7
Afshan Martin <amartin@purepm.co>
2024-08-10 23:50:04 -0600

    avatar component

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,17 +1,18 @@
 <template>
   <div
-    v-if="!isLoading"
     class="flex h-full items-center justify-center align-middle mt-12 text-3xl"
   >
-    Hello {{ user.name }}. You are Logged In!
+    <img
+      class="inline-block rounded-full w-1/3"
+      :src="avatarSrc"
+      alt="avatar"
+    />
   </div>
-
-  <Avatar />
 </template>
 
 <script setup lang="ts">
 import { useAuth0 } from "@auth0/auth0-vue";
-import Avatar from "@components/Avatar.vue";
 
-const { user, isLoading } = useAuth0();
+const { user } = useAuth0();
+const avatarSrc = `/src/assets/avatars/${user?.value.avatar}.jpg`;
 </script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,7 @@
 <template>
-  <div
-    class="flex h-full items-center justify-center align-middle mt-12 text-3xl"
-  >
-    <img
-      class="inline-block rounded-full w-1/3"
-      :src="avatarSrc"
-      alt="avatar"
-    />
-  </div>
+  <Avatar />
 </template>
 
 <script setup lang="ts">
-import { useAuth0 } from "@auth0/auth0-vue";
-
-const { user } = useAuth0();
-const avatarSrc = `/src/assets/avatars/${user?.value.avatar}.jpg`;
+import Avatar from "@components/Avatar.vue";
 </script>
