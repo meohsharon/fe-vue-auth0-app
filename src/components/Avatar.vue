1a8331fea1d14c595eeaa933f402c64a9c41d2d1
Afshan Martin <amartin@purepm.co>
2024-08-11 00:12:34 -0600

    vite specific img src url

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -14,5 +14,5 @@
 import { useAuth0 } from "@auth0/auth0-vue";
 
 const { user } = useAuth0();
-const avatarSrc = `@assets/img/avatars/${user?.value.avatar}.jpg`;
+const avatarSrc = new URL(`/src/assets/img/avatars/${user?.value.avatar}.jpg`, import.meta.url).href;
 </script>
