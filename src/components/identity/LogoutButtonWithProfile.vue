8efe0cd6b9cde6ce068156f3a3404f3e8fa770e6
Afshan Martin <amartin@purepm.co>
2024-08-15 11:20:58 -0600

    burger menu color red

--- src/components/identity/LogoutButtonWithProfile.vue
+++ src/components/identity/LogoutButtonWithProfile.vue
@@ -22,7 +22,7 @@
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="w-[30px]"
+          class="w-[30px] text-red-500"
         >
           <path
             stroke-linecap="round"
