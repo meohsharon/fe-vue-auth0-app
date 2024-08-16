a56128e674a5b35e6b87b742002e60c3cd85eb82
huska0707 <hussaink001haled@gmail.com>
2024-08-16 16:28:06 -0500

    clean up storytelling style and structure

--- src/views/StoryTelling.vue
+++ src/views/StoryTelling.vue
@@ -1,50 +1,29 @@
 <template>
-  <div
-    class="w-full h-[605px] sm:h-[595px] xl:h-[695px] flex flex-col justify-center items-center relative -mt-[60px] z-50 bg-[#0c2977]"
-  >
+  <div class="storytelling-container">
     <img
       v-if="showContent"
-      src="@assets/img/story-telling/map.png"
+      src="@/assets/img/story-telling/map.png"
       alt="World Map"
-      class="w-full sm:w-[700px] xl:w-[1000px] absolute md:top-[58px]"
+      class="storytelling-map"
     />
 
-    <div
-      class="absolute -bottom-[20px] sm:bottom-[50px] xl:-bottom-[23px] left-1/2 -translate-x-1/2 pb-28 sm:pb-0 w-[400px] sm:w-[640px] xl:w-[740px]"
-    >
-      <div class="relative w-full flex justify-center">
+    <div class="storytelling-content">
+      <div class="text-image-container">
         <img
           src="@/assets/img/story-telling/textImg.png"
-          alt="text Image"
-          class="h-[151px] sm:h-[190px] xl:h-[230px] w-[640px] xl:w-[740px]"
+          alt="Text Image"
+          class="text-image"
         />
-        <img
-          src="@/assets/img/story-telling/boy.png"
-          alt="text Image"
-          class="absolute -top-[63px] sm:-top-[183px] xl:-top-[211px] right-[83px] sm:right-[90px] xl:right-[85px] w-[80px] sm:w-[200px] xl:w-[245px] h-[100px] sm:h-[230px] xl:h-[270px]"
-        />
-        <p
-          v-if="showContent"
-          class="absolute top-[25px] sm:top-[30px] md:top-[32px] xl:top-[37px] left-[95px] sm:left-[123px] xl:left-[118px] text-xs sm:text-lg xl:text-[25px] pl-0 sm:pl-[30px] text-white font-medium"
-        >
-          Sharon
-        </p>
-        <p
-          v-if="showContent"
-          class="absolute top-[65px] sm:top-[84px] xl:top-[95px] left-[20px] sm:left-[55px] xl:left-[45px] text-[10px] sm:text-[14px] xl:text-[17px] w-[335px] sm:w-[478px] xl:w-[580px] leading-5 sm:leading-6 xl:leading-8"
-        >
+        <img src="@/assets/img/story-telling/boy.png" alt="Boy Image" class="boy-image" />
+        <p v-if="showContent" class="storytelling-title">Sharon</p>
+        <p v-if="showContent" class="storytelling-description">
           Pick your location on the map and complete your address! Make sure to confirm
           all information is correct and once that’s done, let’s go plant your tree.
         </p>
       </div>
     </div>
 
-    <button
-      @click="showContent = true"
-      class="w-[75px] xl:w-[100px] h-[40px] xl::h-[50px] border-white border-[1px] xl:border-2 border-solid rounded-tr-[15px] rounded-bl-[15px] xl:font-medium text-base xl:text-[22px] text-white absolute bottom-5 sm:bottom-1 xl:bottom-6 right-5 sm:right-2 xl:right-16"
-    >
-      Next
-    </button>
+    <button @click="showContent = true" class="storytelling-button">Next</button>
   </div>
 </template>
 
@@ -60,5 +39,39 @@ export default {
 </script>
 
 <style scoped>
-/* You can keep your existing styles here or add new ones as needed */
+.storytelling-container {
+  @apply w-full h-[605px] sm:h-[595px] xl:h-[695px] flex flex-col justify-center items-center relative -mt-[60px] z-50 bg-[#0c2977];
+}
+
+.storytelling-map {
+  @apply w-full sm:w-[700px] xl:w-[1000px] absolute md:top-[58px];
+}
+
+.storytelling-content {
+  @apply absolute -bottom-[20px] sm:bottom-[50px] xl:-bottom-[23px] left-1/2 -translate-x-1/2 pb-28 sm:pb-0 w-[400px] sm:w-[640px] xl:w-[740px];
+}
+
+.text-image-container {
+  @apply relative w-full flex justify-center;
+}
+
+.text-image {
+  @apply h-[151px] sm:h-[190px] xl:h-[230px] w-[640px] xl:w-[740px];
+}
+
+.boy-image {
+  @apply absolute -top-[63px] sm:-top-[183px] xl:-top-[211px] right-[83px] sm:right-[90px] xl:right-[85px] w-[80px] sm:w-[200px] xl:w-[245px] h-[100px] sm:h-[230px] xl:h-[270px];
+}
+
+.storytelling-title {
+  @apply absolute top-[25px] sm:top-[30px] md:top-[32px] xl:top-[37px] left-[95px] sm:left-[123px] xl:left-[118px] text-xs sm:text-lg xl:text-[25px] pl-0 sm:pl-[30px] text-white font-medium;
+}
+
+.storytelling-description {
+  @apply absolute top-[65px] sm:top-[84px] xl:top-[95px] left-[20px] sm:left-[55px] xl:left-[45px] text-[10px] sm:text-[14px] xl:text-[17px] w-[335px] sm:w-[478px] xl:w-[580px] leading-5 sm:leading-6 xl:leading-8;
+}
+
+.storytelling-button {
+  @apply w-[75px] xl:w-[100px] h-[40px] xl:h-[50px] border-white border-[1px] xl:border-2 border-solid rounded-tr-[15px] rounded-bl-[15px] xl:font-medium text-base xl:text-[22px] text-white absolute bottom-5 sm:bottom-1 xl:bottom-6 right-5 sm:right-2 xl:right-16;
+}
 </style>
