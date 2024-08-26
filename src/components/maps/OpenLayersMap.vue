e3f4610a773db61552a360e136510a48bb08d1e7
huska77 <hussaink001haled@gmail.com>
2024-08-26 15:43:01 -0500

    show marker on map

--- src/components/maps/OpenLayersMap.vue
+++ src/components/maps/OpenLayersMap.vue
@@ -1,17 +1,25 @@
 <template>
   <!-- Source: https://vue3openlayers.netlify.app/ -->
-  <ol-map ref="map" style="height: 70vh" :controls="[]">
-    <ol-view
-      ref="view"
-      :center="center"
-      :zoom="zoom"
-      :projection="projection"
-    />
+  <ol-map ref="map" style="height: 70vh" :controls="[]" @click="onMapClick">
+    <ol-view ref="view" :center="center" :zoom="zoom" :projection="projection" />
 
     <ol-tile-layer>
       <ol-source-osm />
     </ol-tile-layer>
     <ol-attribution-control />
+
+    <ol-vector-layer v-if="coordinate">
+      <ol-source-vector>
+        <ol-feature>
+          <ol-geom-point :coordinates="coordinate"></ol-geom-point>
+          <ol-style>
+            <ol-style-box>
+              <ol-style-icon :src="hereIcon" scale="0.25" />
+            </ol-style-box>
+          </ol-style>
+        </ol-feature>
+      </ol-source-vector>
+    </ol-vector-layer>
 
     <ol-geolocation :projection="projection" @change:position="geoLocChange">
       <template>
@@ -43,6 +51,14 @@ const projection = ref("EPSG:4326");
 const view = ref<View>();
 const map = ref(null);
 const position = ref([]);
+
+const coordinate = ref(null);
+
+const onMapClick = (event) => {
+  const map = event.target;
+  coordinate.value = map.getEventCoordinate(event.originalEvent);
+};
+
 const geoLocChange = (event: ObjectEvent) => {
   position.value = event.target.getPosition();
   view.value?.setCenter(event.target?.getPosition());
