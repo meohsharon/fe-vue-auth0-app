ccbefe77a956d3208d31e237cbdc3874ce7d1dd1
Afshan Martin <amartin@purepm.co>
2024-09-02 14:00:54 -0600

    status

--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="flex-grow text-center">
-    <p class="mt-12 text-sm sm:text-base text-red-600">
-      Hello {{ user.name }}. Complete the following sections to unlock features
-    </p>
+    <div id="intro">
+      <p class="mt-12 text-xs text-red-600">Hello {{ user.name }}</p>
+      <p class="mt-4 text-2xl">Complete Sections to Unlock Features</p>
+    </div>
 
-    <div id="'onboardingFlow'" class="mt-12 mx-12 text-left text-emerald-500">
+    <div id="'onboardingFlow'" class="mt-12 mx-12 text-left">
       <p>
         Number of Sections to complete: {{ Object.keys(onboardingFlow).length }}
       </p>
@@ -12,7 +13,7 @@
       <div id="section" v-for="(sections, key) of onboardingFlow" class="my-8">
         <fieldset class="border border-dashed rounded-md border-gray-300 p-3">
           <legend class="p-2 text-fuchsia-600">{{ key }}</legend>
-          <div class="grid gap-x-8 gap-y-6 underline text-orange-600">
+          <div class="grid gap-x-8 gap-y-6 underline underline-offset-8">
             <p>
               Number of Steps to complete in this section: {{ sections.length }}
             </p>
@@ -27,13 +28,13 @@
               <div id="step" class="pl-8">
                 <div
                   id="questions"
-                  class="flex justify-between border-b-4 border-dotted mb-8"
+                  class="flex justify-between border-b border-solid mb-8"
                 >
                   <pre class="text-pink-600">
 Questions to Answer: {{ steps.questions }}</pre
                   >
                   <button
-                    class="bg-pink-500 hover:bg-gray-300 text-white hover:text-pink-500 hover:border-transparent rounded text-xs px-4 m-4 h-12"
+                    class="bg-pink-400 hover:bg-gray-300 text-white hover:text-pink-500 hover:border-transparent rounded text-xs px-4 m-4 h-12 w-44"
                   >
                     Complete This Step
                   </button>
@@ -41,13 +42,13 @@ Questions to Answer: {{ steps.questions }}</pre
 
                 <div
                   id="validations"
-                  class="flex justify-between border-b-4 border-dotted mb-8"
+                  class="flex justify-between border-b border-solid mb-8"
                 >
                   <pre class="text-cyan-500">
 Validations to Pass: {{ steps.validations_required }}</pre
                   >
                   <button
-                    class="bg-cyan-500 hover:bg-gray-300 text-white hover:text-cyan-500 hover:border-transparent rounded text-xs px-4 m-4 h-12"
+                    class="bg-cyan-500 hover:bg-gray-300 text-white hover:text-cyan-500 hover:border-transparent rounded text-xs px-4 m-4 h-12 w-44"
                   >
                     Complete This Step
                   </button>
@@ -55,15 +56,18 @@ Validations to Pass: {{ steps.validations_required }}</pre
 
                 <div
                   id="features"
-                  class="flex justify-between border-b-4 border-dotted mb-8"
+                  class="flex justify-between border-b border-solid mb-8"
                 >
-                  <pre class="text-green-500">
+                  <pre class="text-orange-500">
 Features to Unlock:: {{ steps.unlocked_features }}</pre
                   >
                   <button
-                    class="bg-green-600 hover:bg-gray-300 text-white hover:text-green-600 hover:border-transparent rounded text-xs px-4 m-4 h-12"
+                    class="bg-orange-400 hover:bg-gray-300 text-white hover:text-orange-600 hover:border-transparent rounded text-xs px-4 m-4 h-12 w-44"
                   >
-                    Complete This Step
+                    <div class="flex justify-between">
+                      <span> Status:</span>
+                      <LockClosedIcon class="w-4 ml-4"/>
+                    </div>
                   </button>
                 </div>
               </div>
@@ -79,6 +83,7 @@ Features to Unlock:: {{ steps.unlocked_features }}</pre
 import { useAuth0 } from "@auth0/auth0-vue";
 import { onboardingFlow } from "@helpers/dataModel/index";
 import Avatar from "@components/Avatar.vue";
+import { LockClosedIcon } from "@heroicons/vue/20/solid";
 
 const { user } = useAuth0();
 </script>
