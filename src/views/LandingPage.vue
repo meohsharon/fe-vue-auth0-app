445b7019a243a81eb185ab0d418760c0f9d7b94a
Afshan Martin <amartin@purepm.co>
2024-09-06 17:16:45 -0600

    dont show story if logged in

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -6,9 +6,17 @@
     alt="Hidden Image for Auth0 Login"
   />
 
-  <Story class="mx-auto sm:w-full md:w-full lg:w-full xl:w-[65%]" />
+  <Story v-if="!isAuthenticated" class="mx-auto sm:w-full md:w-full lg:w-full xl:w-[65%]" />
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+import { useAuth0 } from "@auth0/auth0-vue";
 import Story from "@components/story/Story.vue";
+
+const { isAuthenticated } = useAuth0();
+if (isAuthenticated.value === true) {
+  const router = useRouter();
+  router.replace({ path: "/home" })
+}
 </script>
