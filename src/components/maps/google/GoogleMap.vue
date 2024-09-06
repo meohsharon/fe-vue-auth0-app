4b861d2c5e31681945dcd8d3056cd2841d8e5f3f
Afshan Martin <amartin@purepm.co>
2024-09-06 09:44:15 -0600

    event.stop to prevent default

--- src/components/maps/google/GoogleMap.vue
+++ src/components/maps/google/GoogleMap.vue
@@ -139,6 +139,8 @@ const showContextMenu = (event) => {
   showMenu.value = true;
   menuX.value = event.pixel.x;
   menuY.value = event.pixel.y;
+  event.stop();
+  // mapRef.value?.map.stop()
 };
 
 const closeContextMenu = () => {
