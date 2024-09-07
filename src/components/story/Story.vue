d86273ccd4bed5fe13a2eb683e00bfa3634a578e
Mahnoor <160244492+MahnoorScript@users.noreply.github.com>
2024-09-07 14:28:11 +0500

    mobilepics branch (photos included)

--- src/components/story/Story.vue
+++ src/components/story/Story.vue
@@ -1,17 +1,16 @@
 <template>
-  <!-- Carousel Source: https://ismail9k.github.io/vue3-carousel/getting-started.html -->
   <Carousel @slide-end="handleSlideEnd">
-    <Slide v-for="slide in totalSlides" :key="slide">
-      <div v-if="slide === 1" id="sceneOne">
-        <SceneOne :src="imgSrc(slide)" />
+    <Slide v-for="(slide, index) in totalSlides" :key="index">
+      <div v-if="index === 0" id="sceneOne">
+        <SceneOne :src="imgSrc(index + 1)" />
       </div>
 
-      <div v-else-if="slide === totalSlides" id="sceneFour">
+      <div v-else-if="index === totalSlides - 1" id="sceneFour">
         <AvatarPicker />
       </div>
 
       <div v-else id="otherScenes">
-        <img :src="imgSrc(slide)" :alt="`scene${slide}`" />
+        <img :src="imgSrc(slide + 1)" :alt="`scene${index + 1}`" />
       </div>
     </Slide>
 
@@ -23,8 +22,7 @@
 
 <script setup lang="ts">
 import "vue3-carousel/dist/carousel.css";
-
-import { defineComponent, ref } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { Carousel, Navigation, Slide } from "vue3-carousel";
 import SceneOne from "./SceneOne.vue";
@@ -34,23 +32,31 @@ const router = useRouter();
 const totalSlides = 4;
 let showNavigation = ref(true);
 
+
+const isMobile = () => {
+  return window.innerWidth <= 768;
+};
+
 const imgSrc = (index: number) => {
-  return new URL(`/src/assets/game/scene${index}.png`, import.meta.url).href;
+  if (isMobile()) {
+    const mobileSrc = new URL(`/src/assets/game/mobile-friendly-designs(${index}).png`, import.meta.url).href;
+    console.log("Mobile image source:", mobileSrc);
+    return mobileSrc;
+  } else {
+    const desktopSrc = new URL(`/src/assets/game/scene${index}.png`, import.meta.url).href;
+    console.log("Desktop image source:", desktopSrc); 
+    return desktopSrc;
+  }
 };
 
 const handleSlideEnd = (data) => {
   if (data.currentSlideIndex === totalSlides - 1) {
     showNavigation.value = false;
-    // setTimeout(() => router.replace({ path: "/pick-avatar" }), 5000);
   }
 };
 
-defineComponent({
-  name: "Story",
-  components: {
-    Carousel,
-    Slide,
-    Navigation,
-  },
+window.addEventListener('resize', () => {
+  // Trigger Vue reactivity on window resize to check for mobile screen size
+  isMobile();
 });
 </script>
